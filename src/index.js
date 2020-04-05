f69862dd4e294733cad8940f48a6f4bbb5adf7e5
Aleksei Arro <polsck@gmail.com>
2020-04-05 22:56:19 +0300

    Tasklist TODO added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+/** TODO:
+ * Tasklist
+ * ========
+ * Rework with hooks
+ * Display the location for each move in the format (col, row) in the move history list.
+ * Add a toggle button that lets you sort the moves in either ascending or descending order.
+ * When someone wins, highlight the three squares that caused the win.
+ * When no one wins, display a message about the result being a draw.
+ */
+
 import './common.scss';
 
 import initApp from './init-app.jsx';
