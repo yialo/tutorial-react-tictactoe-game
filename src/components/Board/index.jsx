725ef43277360571e0d0507c19e7b6e01fa28489
Aleksei Arro <polsck@gmail.com>
2020-04-06 14:56:34 +0300

    Board component reworked as functional

--- src/components/Board/index.jsx
+++ src/components/Board/index.jsx
@@ -4,21 +4,20 @@ import React from 'react';
 import Square from '../Square/index.jsx';
 import BoardSize from '../../utils/board-size.js';
 
-export default class Board extends React.PureComponent {
-  render() {
-    return (
-      <div className={['board'].concat(this.props.classNames ?? []).join(' ')}>
-        {new Array(BoardSize.getTotal()).fill(null).map((_it, i) => (
-          <Square
-            key={i}
-            classNames="board__field"
-            value={this.props.squares[i]}
-            onClick={() => {
-              this.props.onClick(i);
-            }}
-          />
-        ))}
-      </div>
-    );
-  }
+export default function Board(props) {
+  const { classNames, squares, onClick } = props;
+  return (
+    <div className={['board'].concat(classNames ?? []).join(' ')}>
+      {new Array(BoardSize.getTotal()).fill(null).map((_it, i) => (
+        <Square
+          key={i}
+          classNames="board__field"
+          value={squares[i]}
+          onClick={() => {
+            onClick(i);
+          }}
+        />
+      ))}
+    </div>
+  );
 }
